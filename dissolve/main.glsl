592e33866d34eed39c478edb3164201b613bd37e
nathalie_raffray <nathalie@3dverse.com>
2023-08-19 00:46:06 -0400

    dissolve

--- dissolve/main.glsl
+++ dissolve/main.glsl
@@ -1,82 +1,61 @@
+
 //---------------------------------------------------------------------------
-float character(int n, vec2 p)
-{
-	p = floor(p*vec2(-4.0, 4.0) + 2.5);
-    if (clamp(p.x, 0.0, 4.0) == p.x)
-	{
-        if (clamp(p.y, 0.0, 4.0) == p.y)
-		{
-        	int a = int(round(p.x) + 5.0 * round(p.y));
-			if (((n >> a) & 1) == 1) return 1.0;
-		}
-    }
-	return 0.0;
+vec3 permute(vec3 x) { return mod(((x*34.0)+1.0)*x, 289.0); }
+
+//---------------------------------------------------------------------------
+float simplexNoise(vec2 v){
+    const vec4 C = vec4(0.211324865405187, 0.366025403784439,
+            -0.577350269189626, 0.024390243902439);
+    vec2 i  = floor(v + dot(v, C.yy) );
+    vec2 x0 = v -   i + dot(i, C.xx);
+    vec2 i1;
+    i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+    vec4 x12 = x0.xyxy + C.xxzz;
+    x12.xy -= i1;
+    i = mod(i, 289.0);
+    vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
+    + i.x + vec3(0.0, i1.x, 1.0 ));
+    vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy),
+        dot(x12.zw,x12.zw)), 0.0);
+    m = m*m ;
+    m = m*m ;
+    vec3 x = 2.0 * fract(p * C.www) - 1.0;
+    vec3 h = abs(x) - 0.5;
+    vec3 ox = floor(x + 0.5);
+    vec3 a0 = x - ox;
+    m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
+    vec3 g;
+    g.x  = a0.x  * x0.x  + h.x  * x0.y;
+    g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+    return 130.0 * dot(m, g);
 }
 
 //---------------------------------------------------------------------------
-vec4 dissolve(in vec2 pix, in vec4 col)
+vec4 dissolve(in vec2 pix, in vec4 col, in float timeToDissolveInS)
 {
-    int n = 4096;
+    float time = 5 - ConstData.elapsedTime;
+    time = time < 0 ? 0 : time;
+    //const float time = ConstData.elapsedTime;
 
-    float gray = 0.3 * col.r + 0.59 * col.g + 0.11 * col.b * abs(sin(ConstData.elapsedTime)) * 10;
+    float speed = 1.5;
+    float factor = 1 - (time * speed);
+    int noiseScale = 80;
 
-    // limited character set
-    if (gray > 0.2) n = 65600;    // :
-	if (gray > 0.3) n = 163153;   // *
-	if (gray > 0.4) n = 15255086; // o
-	if (gray > 0.5) n = 13121101; // &
-	if (gray > 0.6) n = 15252014; // 8
-	if (gray > 0.7) n = 13195790; // @
-	if (gray > 0.8) n = 11512810; // #
+    //vec4 color = vec4(0.2, 0.6, 0.4, 1);
+    vec4 color = col;
+    vec4 dissolveColor = vec4(0.3, 0.3, 1, 1);
 
-    // full character set including A-Z and 0-9
+    float noise = simplexNoise(pix / noiseScale);
 
-    if (gray > 0.0233) n = 4096;
-    if (gray > 0.0465) n = 131200;
-    if (gray > 0.0698) n = 4329476;
-    if (gray > 0.0930) n = 459200;
-    if (gray > 0.1163) n = 4591748;
-    if (gray > 0.1395) n = 12652620;
-    if (gray > 0.1628) n = 14749828;
-    if (gray > 0.1860) n = 18393220;
-    if (gray > 0.2093) n = 15239300;
-    if (gray > 0.2326) n = 17318431;
-    if (gray > 0.2558) n = 32641156;
-    if (gray > 0.2791) n = 18393412;
-    if (gray > 0.3023) n = 18157905;
-    if (gray > 0.3256) n = 17463428;
-    if (gray > 0.3488) n = 14954572;
-    if (gray > 0.3721) n = 13177118;
-    if (gray > 0.3953) n = 6566222;
-    if (gray > 0.4186) n = 16269839;
-    if (gray > 0.4419) n = 18444881;
-    if (gray > 0.4651) n = 18400814;
-    if (gray > 0.4884) n = 33061392;
-    if (gray > 0.5116) n = 15255086;
-    if (gray > 0.5349) n = 32045584;
-    if (gray > 0.5581) n = 18405034;
-    if (gray > 0.5814) n = 15022158;
-    if (gray > 0.6047) n = 15018318;
-    if (gray > 0.6279) n = 16272942;
-    if (gray > 0.6512) n = 18415153;
-    if (gray > 0.6744) n = 32641183;
-    if (gray > 0.6977) n = 32540207;
-    if (gray > 0.7209) n = 18732593;
-    if (gray > 0.7442) n = 18667121;
-    if (gray > 0.7674) n = 16267326;
-    if (gray > 0.7907) n = 32575775;
-    if (gray > 0.8140) n = 15022414;
-    if (gray > 0.8372) n = 15255537;
-    if (gray > 0.8605) n = 32032318;
-    if (gray > 0.8837) n = 32045617;
-    if (gray > 0.9070) n = 33081316;
-    if (gray > 0.9302) n = 32045630;
-    if (gray > 0.9535) n = 33061407;
-    if (gray > 0.9767) n = 11512810;
+    // if factor < noise then 0, otherwise 1
+    float sn1 = step(noise, factor);
+    float sn2 = step(noise, factor + 0.1) - sn1;
 
-	vec2 p = mod(pix/4.0, 2.0) - vec2(1.0);
+    dissolveColor *= sn2 * color.a;
+    color.a *= sn1;
+    
+    //return vec4(1.0, 0, 0, 1.0);
+    return dissolveColor + color;
 
-	col = col*character(n, p);
-
-	return vec4(col.xyz, 1.0);
+	//return vec4(col.xyz, 1.0);
 }
